af7e5500e7c207fb0b174eda166894ffac639e24
chyuhung <977599667@qq.com>
2022-06-08 17:46:50 +0800

    重构代码

--- main.go
+++ main.go
@@ -9,9 +9,24 @@ import (
 )
 
 func main() {
-	port := "27149"
-	go server.Run()
 
+	go server.Run()
+	cmd := startBrowser()
+	chanSignal := make(chan os.Signal, 1)
+	signal.Notify(chanSignal, os.Interrupt)
+	// 监听中断信号
+	select {
+	case <-chanSignal:
+		err := cmd.Process.Kill()
+		if err != nil {
+			fmt.Println("浏览器关闭失败，请手动关闭！")
+			fmt.Println(err)
+		}
+	}
+}
+
+func startBrowser() *exec.Cmd {
+	port := "27149"
 	url := "http://127.0.0.1:" + port + "/static/index.html"
 	chromePath := "C:\\Program Files (x86)\\Microsoft\\EdgeCore\\102.0.1245.30\\msedge.exe"
 	cmd := exec.Command(chromePath, "--app="+url)
@@ -20,17 +35,5 @@ func main() {
 		fmt.Println("浏览器启动失败，请手动访问：", url)
 		fmt.Println(err)
 	}
-
-	chanSignal := make(chan os.Signal, 1)
-	signal.Notify(chanSignal, os.Interrupt)
-	// 监听终端信号
-	select {
-	case <-chanSignal:
-		err = cmd.Process.Kill()
-		if err != nil {
-			fmt.Println("浏览器关闭失败，请手动关闭！")
-			fmt.Println(err)
-		}
-	}
-
+	return cmd
 }
